e80d130b8c5841b5d4a018fb98a317f353dbd566
Gaurav Kumar <109330154+akashsInterra@users.noreply.github.com>
2023-01-28 19:44:42 +0530

    second commit

--- src/main/java/com/driver/Message.java
+++ src/main/java/com/driver/Message.java
@@ -6,6 +6,15 @@ public class Message {
     private int id;
     private String content;
     private Date timestamp;
+    public Message(int id){
+        this.id=id;
+        this.timestamp=new Date();
+    }
+    public Message(int id,String content){
+        this.content=content;
+        this.id=id;
+        this.timestamp=new Date();
+    }
     public Message(int id,String content,Date timestamp){
         this.id=id;
         this.content=content;
